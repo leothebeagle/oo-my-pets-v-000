8641e50ae9a9373fe7de77a310518f9633f2842b
Aziz Kosber <a.kosber@gmail.com>
2018-08-04 05:07:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -42,6 +42,14 @@ class Owner
     pets[:dogs].each { |dog| dog.mood = "happy"}
   end
 
+  def play_with_cats
+    pets[:cats].each { |cat| cat.mood = "happy" }
+  end
+
+  def feed_fish
+
+  end
+
 
 
 end
